19c664b87bd8abab5b1283b9d8a86edfa6984a67
Shubham Nimje <shubhamsnimje@gmail.com>
2025-06-30 13:40:18 +0530

    30-06-2025

--- app1/views.py
+++ app1/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 
 def homePage(request):
-    response = render(request, 'index.html', context={})
+    # ================v1======================
+    email_list=['nareshit@nareshit.com',
+    'ramesh@gmail.com',
+    'kishore@gamil.com',
+    'rajesh@gmail.com']
+    # =================v2=====================
+    marks={'naresh':[40,50,60],
+    'ramesh':[60,70,80],
+    'suresh':[70,80,90]}
+    # =================v3=====================
+    A=[10,20,30,40,50]
+    B={'x':10,'y':20,'z':30}
+    response = render(request, 'index.html', context={'email_list':email_list,'marks':marks,'A':A,'B':B})
     return response
-def courses(request):
-    response=render(request,"courses.html")
-    return response 
+
 def about(request):
     response=render(request,"about.html")
     return response 
+
 def contacts(request):
-    response=render(request,"contacts.html")
+    a = 'ironman'
+    b = 'thor'
+    c =  'deadpool'
+    response=render(request,"contacts.html", context={'a':a,'b':b,'c':c})
     return response  
+def courses(request):
+    a = 'python'
+    b = 'django'
+    c = 'flask'
+    response=render(request,"courses.html", context={'a':a,'b':b,'c':c})
+    return response 
