2240c89f7dae6a654748583c22fbb12077f0906c
Shubham Nimje <shubhamsnimje@gmail.com>
2025-08-01 12:51:04 +0530

    ccookies

--- cookies/urls.py
+++ cookies/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', home),
     path('find/',view2),
     path('read/',view1),
+
+    path('addcart/',addproduct),
+    path('viewcart/',viewcart),
+    path('updatecart/',updateproduct),
+    path('deletecart/',deleteproduct),
 ]
--- cookies/views.py
+++ cookies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from cookies.forms import ProductForm
 from django.http import HttpResponse 
 
 # Create your views here.
@@ -19,3 +20,47 @@ def view2(request):
 def home(request):
     response=render(request,"home.html",context={})
     return response 
+
+def addproduct(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        qty=request.POST.get('qty')
+        res=render(request,"home.html",context={})
+        res.set_cookie(name,qty)
+        return res 
+    form=ProductForm()
+    res=render(request,"addproduct_temp.html",context={'form':form})
+    return res
+
+
+def viewcart(request):
+    products={}
+    for name,qty in request.COOKIES.items():
+        if name!='csrftoken':
+            products[name]=qty 
+
+
+    res=render(request,"viewcart_temp.html",context={'products':products})
+    return res 
+
+
+def updateproduct(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        qty=request.POST.get('qty')
+        res=render(request,"home.html",context={})
+        res.set_cookie(name,qty)
+        return res 
+    form=ProductForm()
+    res=render(request,"updateproduct_temp.html",context={'form':form})
+    return res
+def deleteproduct(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        res=render(request,"home.html",context={})
+        res.delete_cookie(name)
+        return res 
+    form=ProductForm()
+    res=render(request,"deleteproduct_temp.html",context={'form':form})
+    return res
+
